b6e548688a0ac7b19263e404aee3027338c4de95
Mkabil123 <mkabil0210@gmail.com>
2025-07-09 10:29:53 +0530

    Add initial project files and structure for SwiftDemoProject

---
+++ SwiftDemoProject/SwiftDemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoProjectApp.swift
+//  SwiftDemoProject
+//
+//  Created by KARTHI on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDemoProjectTests/SwiftDemoProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftDemoProjectTests.swift
+//  SwiftDemoProjectTests
+//
+//  Created by KARTHI on 19/06/25.
+//
+
+import Testing
+
+struct SwiftDemoProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
